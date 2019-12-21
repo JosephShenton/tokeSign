d43b6fdb2bdcb4150dd326c334a63f19c18018df
Joseph Shenton <joe@jjs.digital>
2019-12-21 11:46:36 +1100

    tests

--- project/tests.php
+++ project/tests.php
@@ -2,4 +2,19 @@
 	set_include_path(get_include_path() . PATH_SEPARATOR . 'libraries');
 
 	include('phpseclib/File/X509.php');
+
+	$filename = 'resources/example_files/123.p12';
+	$password = '123';
+	$results = array();
+	$worked = openssl_pkcs12_read(file_get_contents($filename), $results, $password));
+	if($worked) {
+	    echo '<pre>', print_r($results, true), '</pre>';
+	} else {
+	    echo openssl_error_string();
+	}
+
+	// $x509 = new File_X509();
+	// $cert = $x509->loadX509();
+
+	// print_r($cert);
 ?>
